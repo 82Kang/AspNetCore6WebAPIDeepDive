afcd588d00627112ef472a5fb73f4c78329f6ba7
bayantt <bayantkang@gmail.com>
2023-03-09 23:55:31 +0530

    Indentation correction in AuthorsController

--- Starter files/CourseLibrary.API/Controllers/AuthorsController.cs
+++ Starter files/CourseLibrary.API/Controllers/AuthorsController.cs
@@ -9,57 +9,57 @@ namespace CourseLibrary.API.Controllers;
 [ApiController] 
 public class AuthorsController : ControllerBase
 {
-    private readonly ICourseLibraryRepository _courseLibraryRepository;
-    private readonly IMapper _mapper;
+  private readonly ICourseLibraryRepository _courseLibraryRepository;
+  private readonly IMapper _mapper;
 
-    public AuthorsController(
-        ICourseLibraryRepository courseLibraryRepository,
-        IMapper mapper)
-    {
-        _courseLibraryRepository = courseLibraryRepository ??
-            throw new ArgumentNullException(nameof(courseLibraryRepository));
-        _mapper = mapper ??
-            throw new ArgumentNullException(nameof(mapper));
-    }
+  public AuthorsController(
+      ICourseLibraryRepository courseLibraryRepository,
+      IMapper mapper)
+  {
+    _courseLibraryRepository = courseLibraryRepository ??
+      throw new ArgumentNullException(nameof(courseLibraryRepository));
+    _mapper = mapper ??
+      throw new ArgumentNullException(nameof(mapper));
+  }
 
-    [HttpGet("api/authors")] 
+  [HttpGet("api/authors")] 
     public async Task<ActionResult<IEnumerable<AuthorDto>>> GetAuthors()
     { 
-        // get authors from repo
-        var authorsFromRepo = await _courseLibraryRepository
-            .GetAuthorsAsync(); 
+      // get authors from repo
+      var authorsFromRepo = await _courseLibraryRepository
+        .GetAuthorsAsync(); 
 
-        // return them
-        return Ok(_mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo));
+      // return them
+      return Ok(_mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo));
     }
 
-    [HttpGet("api/authors/{authorId}", Name = "GetAuthor")]
-    public async Task<ActionResult<AuthorDto>> GetAuthor(Guid authorId)
+  [HttpGet("api/authors/{authorId}", Name = "GetAuthor")]
+  public async Task<ActionResult<AuthorDto>> GetAuthor(Guid authorId)
+  {
+    // get author from repo
+    var authorFromRepo = await _courseLibraryRepository.GetAuthorAsync(authorId);
+
+    if (authorFromRepo == null)
     {
-        // get author from repo
-        var authorFromRepo = await _courseLibraryRepository.GetAuthorAsync(authorId);
-
-        if (authorFromRepo == null)
-        {
-            return NotFound();
-        }
-
-        // return author
-        return Ok(_mapper.Map<AuthorDto>(authorFromRepo));
+      return NotFound();
     }
 
-    [HttpPost("api/authors")]
-    public async Task<ActionResult<AuthorDto>> CreateAuthor(AuthorDto author)
-    {
-        var authorEntity = _mapper.Map<Entities.Author>(author);
+    // return author
+    return Ok(_mapper.Map<AuthorDto>(authorFromRepo));
+  }
 
-        _courseLibraryRepository.AddAuthor(authorEntity);
-        await _courseLibraryRepository.SaveAsync();
+  [HttpPost("api/authors")]
+  public async Task<ActionResult<AuthorDto>> CreateAuthor(AuthorDto author)
+  {
+    var authorEntity = _mapper.Map<Entities.Author>(author);
 
-        var authorToReturn = _mapper.Map<AuthorDto>(authorEntity);
+    _courseLibraryRepository.AddAuthor(authorEntity);
+    await _courseLibraryRepository.SaveAsync();
 
-        return CreatedAtRoute("GetAuthor",
-            new { authorId = authorToReturn.Id },
-            authorToReturn);
-    }
+    var authorToReturn = _mapper.Map<AuthorDto>(authorEntity);
+
+    return CreatedAtRoute("GetAuthor",
+        new { authorId = authorToReturn.Id },
+        authorToReturn);
+  }
 }
