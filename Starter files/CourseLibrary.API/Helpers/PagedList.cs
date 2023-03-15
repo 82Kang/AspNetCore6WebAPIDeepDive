ba4a3a27200f4835771952096410297e5f45848a
bayantt <bayantkang@gmail.com>
2023-03-15 16:20:00 +0530

    Added PagedList for paging and resource parameters missed in previous commit.

---
+++ Starter files/CourseLibrary.API/Helpers/PagedList.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseLibrary.API.Helpers;
+
+public class PagedList<T> : List<T>
+{
+  public int CurrentPage { get; private set; }
+  public int TotalPages { get; private set; }
+  public int PageSize { get; private set; }
+  public int TotalCount { get; private set; } // how many items are there
+
+  public bool HasNext => CurrentPage > 1;
+  public bool HasPrevious => CurrentPage < TotalPages;
+
+  public PagedList(List<T> items, int count, int pageNumber, int pageSize)
+  {
+    CurrentPage = pageNumber;
+    PageSize = pageSize;
+    TotalCount = count;
+    TotalPages = (int) Math.Ceiling (count / (double)pageSize);
+    AddRange(items);
+  }
+
+  public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageNumber, int pageSize)
+  {
+    var count = source.Count();
+    var items = await source.Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+
+    return new PagedList<T> (items, count, pageNumber, pageSize);
+  }
+}
